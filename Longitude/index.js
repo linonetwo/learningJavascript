321861f597f90dc10205229dda169aae7a44550b
lin onetwo <lindongwu11@gmail.com>
2016-09-10 20:27:05 +0800

    revert

--- Longitude/index.js
+++ Longitude/index.js
@@ -1,28 +1,84 @@
-function decorateLight(target, key, descriptor) {
-  const method = descriptor.value;
-  let moreAtk = 50;
-  let ret;
-  descriptor.value = (...args)=>{
-    args[1] += moreAtk;
-    ret = method.apply(target, args);
-    return ret;
-  }
-  return descriptor;
+let rules = {
+    mile: {
+        yard: 1760
+    },
+    yard: {
+        feet: 3
+    },
+    feet: {
+        inch: 12
+    },
+    inch: {
+        inch: 1
+    }
 }
 
-class Man{
-  constructor(def = 2,atk = 3,hp = 3){
-    this.init(def,atk,hp);
-  }
+export default class Longtitude {
+    constructor(initialLongtitude, unit) {
+        if (typeof unit !== 'string' || !(unit in rules)) {
+            throw new Error('unit not a string');
+        }
+        this.longtitude = initialLongtitude;
+        this.unit = unit;
+    }
 
-  @decorateArmour
-  @decorateLight
-  init(def,atk,hp){
-    this.def = def; // 防御值
-    this.atk = atk;  // 攻击力
-    this.hp = hp;  // 血量
-  }
-...
+    addRule() {
+
+    }
+
+    getMetaData() {
+        return {
+            longtitude: this.longtitude,
+            unit: this.unit
+        }
+    }
+
+    checkEquality(LongtitudeObject) {
+        const { longtitude: longtitudeToCheck, unit } = LongtitudeObject.getMetaData();
+
+        if (typeof longtitudeToCheck !== 'number') {
+            return false;
+        }
+        if (typeof unit !== 'string') {
+            throw new Error('unit not a string');
+        }
+        if (unit === this.unit) {
+            return this.longtitude === longtitudeToCheck;
+        }
+
+        const methodList = {
+            mile: this.toMile,
+            inch: this.toInch,
+            feet: this.toFeet,
+            yard: this.toYard
+        }
+
+        return methodList[unit](this.longtitude) === longtitudeToCheck;
+    }
+
+
+    to(unitTo, unitFrom = this.unit, longtitude = this.longtitude) {
+        if (unitTo === unitFrom) {
+            return longtitude;
+        }
+
+        for (let unit in rules) {
+            for (let unitDestination in rules[unit]) {
+                if (unitDestination === unitTo) {
+                    return this.to(unit, unitFrom, longtitude * rules[unit][unitDestination]);
+                }
+            }
+        }
+        // 到这说明上面的正向搜索没有搜到，下面开始反向搜索
+        for (let unit in rules) {
+            for (let unitStart in rules[unit]) {
+                if (unitStart === unitFrom) {
+                    return this.to(unitTo, unit, longtitude / rules[unit][unitStart]);
+                }
+            }
+        }
+
+    }
+
 }
-var tony = new Man();
-console.log(`当前状态 ===> ${tony}`);
+
